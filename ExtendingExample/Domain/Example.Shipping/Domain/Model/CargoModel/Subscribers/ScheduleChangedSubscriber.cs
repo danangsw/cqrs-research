6c34835443bb4af5215943e405bd21d033aee204
taufikfadjar <taufikfadjar@live.com>
2016-12-01 16:00:42 +0700

    Cargo Job

---
+++ ExtendingExample/Domain/Example.Shipping/Domain/Model/CargoModel/Subscribers/ScheduleChangedSubscriber.cs
@@ -0,0 +1,34 @@
+﻿using EventFlow.Aggregates;
+using EventFlow.Jobs;
+using EventFlow.Subscribers;
+using Example.Shipping.Domain.Model.CargoModel.Jobs;
+using Example.Shipping.Domain.Model.VoyageModel;
+using Example.Shipping.Domain.Model.VoyageModel.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example.Shipping.Domain.Model.CargoModel.Subscribers
+{
+    public class ScheduleChangedSubscriber :
+        ISubscribeSynchronousTo<VoyageAggregate, VoyageId, VoyageScheduleUpdatedEvent>
+    {
+        private readonly IJobScheduler _jobScheduler;
+
+        public ScheduleChangedSubscriber(
+            IJobScheduler jobScheduler)
+        {
+            _jobScheduler = jobScheduler;
+        }
+
+        public Task HandleAsync(IDomainEvent<VoyageAggregate, VoyageId, VoyageScheduleUpdatedEvent> domainEvent, CancellationToken cancellationToken)
+        {
+            var job = new VerifyCargosForVoyageJob(
+                domainEvent.AggregateIdentity);
+            return _jobScheduler.ScheduleNowAsync(job, cancellationToken);
+        }
+    }
+}
---
+++ ExtendingExample/Domain/Example.Shipping/Domain/Services/IUpdateItineraryService.cs
@@ -0,0 +1,15 @@
+﻿using Example.Shipping.Domain.Model.CargoModel.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example.Shipping.Domain.Services
+{
+    public interface IUpdateItineraryService
+    {
+        Task<Itinerary> UpdateItineraryAsync(Itinerary itinerary, CancellationToken cancellationToken);
+    }
+}
