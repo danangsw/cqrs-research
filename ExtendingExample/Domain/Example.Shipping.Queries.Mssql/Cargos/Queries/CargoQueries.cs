1801898309f08e209d04740bd8f29fed957a9390
taufikfadjar <taufikfadjar@live.com>
2016-12-01 11:27:02 +0700

    Update cargo query handlers

--- ExtendingExample/Domain/Example.Shipping.Queries.Mssql/Cargos/Queries/CargoQueries.cs
+++ ExtendingExample/Domain/Example.Shipping.Queries.Mssql/Cargos/Queries/CargoQueries.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using EventFlow.Core;
+using EventFlow.MsSql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Example.Shipping.Queries.Mssql.Cargos.Queries
 {
-    class CargoQueries
+    public interface ICargoQueries
     {
+        Task<IReadOnlyCollection<CargoReadModel>> GetCargosByCargoIds(IMsSqlConnection _msSqlConnection, string cargoIds, CancellationToken cancellationToken);
+    }
+
+    public class CargoQueries : ICargoQueries
+    {
+        public async Task<IReadOnlyCollection<CargoReadModel>> GetCargosByCargoIds(IMsSqlConnection _msSqlConnection, string cargoIds, CancellationToken cancellationToken)
+        {
+            var readCargoModels = await _msSqlConnection.QueryAsync<CargoReadModel>(
+                Label.Named("mssql-fetch-cargos-read-model"),
+                cancellationToken,
+                "SELECT * FROM [Cargo] WHERE AggregateId in @AggregateIds",
+                 new { AggregateIds = cargoIds })
+                .ConfigureAwait(false);
+
+            return readCargoModels;
+        }
     }
 }
--- ExtendingExample/Domain/Example.Shipping.Queries.Mssql/Cargos/QueryHandlers/GetCargosQueryHandler.cs
+++ ExtendingExample/Domain/Example.Shipping.Queries.Mssql/Cargos/QueryHandlers/GetCargosQueryHandler.cs
@@ -7,14 +7,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using Example.Shipping.Domain.Model.CargoModel;
+using EventFlow.MsSql;
+using Example.Shipping.Queries.Mssql.Cargos.Queries;
+using Example.General.Extension;
+using Example.Shipping.Domain.Model.CargoModel.ValueObjects;
 
 namespace Example.Shipping.Queries.Mssql.Cargos.QueryHandlers
 {
     public class GetCargosQueryHandler : IQueryHandler<GetCargosQuery, IReadOnlyCollection<Cargo>>
     {
-        public Task<IReadOnlyCollection<Cargo>> ExecuteQueryAsync(GetCargosQuery query, CancellationToken cancellationToken)
+        private readonly IMsSqlConnection _msSqlConnection;
+        private ICargoQueries _cargoQueries;
+        private ITransportLegQueries _transportLegQueries;
+
+        public GetCargosQueryHandler(
+            IMsSqlConnection msSqlConnection,
+            ICargoQueries cargoQueries,
+            ITransportLegQueries transportLegQueries)
         {
-            throw new NotImplementedException();
+            _msSqlConnection = msSqlConnection;
+            _cargoQueries = cargoQueries;
+            _transportLegQueries = transportLegQueries;
+        }
+
+        public async Task<IReadOnlyCollection<Cargo>> ExecuteQueryAsync(GetCargosQuery query, CancellationToken cancellationToken)
+        {
+            var inCargoQuery = query.CargoIds.Select(x => x.Value).ToList().CreateInQueryFromListId();
+            Task<IReadOnlyCollection<CargoReadModel>> getCargosByCargoIds = _cargoQueries.GetCargosByCargoIds(_msSqlConnection, inCargoQuery, cancellationToken);
+            Task<IReadOnlyCollection<TransportLegReadModel>> getTransportLegsByCargoIds = _transportLegQueries.GetTransportLegsByCargoIds(_msSqlConnection, inCargoQuery, cancellationToken);
+
+            await Task.WhenAll(getCargosByCargoIds, getTransportLegsByCargoIds);
+
+            return getCargosByCargoIds.Result.Select(x =>
+                x.ToCargo(new CargoId(x.AggregateId),
+                x.ToRoute(),
+                new Itinerary(getTransportLegsByCargoIds.Result.Where(y => y.CargoId == x.AggregateId)
+                               .Select(z => z.ToTransportLeg()).ToList())
+              )).ToList();
         }
     }
 }
